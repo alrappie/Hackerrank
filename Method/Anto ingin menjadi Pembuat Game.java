d5a9cc074164eee7af0a13eb10445603bc40ddeb
alrappie <73277795+alrappie@users.noreply.github.com>
2020-12-12 00:19:21 +0700

    Create Anto ingin menjadi Pembuat Game.java

---
+++ Method/Anto ingin menjadi Pembuat Game.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner GG = new Scanner(System.in);
+        int health = GG.nextInt();
+        int banyakN = GG.nextInt();
+        String[]skill = new String[banyakN];
+        for(int i =0;i<banyakN;i++){
+            skill[i]=GG.next();
+        }
+        print(sisahDarah(banyakN,health,skill));
+    }
+    
+    static int sisahDarah(int n,int darah,String[] data){
+        for(int i=0;i<n;i++){
+            if (data[i].equalsIgnoreCase("punch")){
+                darah-=30;
+                // System.out.println(darah);
+            }else{
+                darah-=(darah*10)/100;
+                // System.out.println(darah);
+            }
+        }
+        return darah;
+    }
+    
+    static void print(int data){
+        if (data<=0){
+            System.out.println("Monster mati");
+        }else
+            System.out.println(data);
+    }
+}
