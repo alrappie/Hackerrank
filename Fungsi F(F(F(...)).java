3fafef907732afaff827130d7b57cdf2b4093549
alrappie <73277795+alrappie@users.noreply.github.com>
2020-12-11 20:09:48 +0700

    Create Fungsi F(F(F(...)).java

---
+++ Fungsi F(F(F(...)).java
@@ -0,0 +1,22 @@
+package com.tutorial;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner GG = new Scanner(System.in);
+        int fungsiN = GG.nextInt();
+        int a = GG.nextInt();
+        int b = GG.nextInt();
+        int awal = GG.nextInt();
+        cetak(fungsi(fungsiN,a,b,awal));
+    }
+    static int fungsi(int x, int a, int b, int awal){
+        int hasil = a*awal+b;
+        if (x==1)return hasil;
+        return fungsi(x-1,a,b,hasil);
+    }
+    static void cetak(int cetakan){
+        System.out.println(cetakan);
+    }
+}
